dd20dfc9b192f1f12c1e545f78186b995718f8ab
Arya-Wahyu-Pratma <2381055@unai.edu>
2024-09-15 22:14:18 +0700

    Tugas 1

--- Soal_3.java
+++ Soal_3.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 public class Soal_3 {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan teks: ");
         String teks = input.nextLine();
-
+        int jumlahkata = countWords(teks);
+        System.out.println("Jumlah kata :" + jumlahkata);
+    }
+    public static int countWords(String teks){
+        int jumlahkata = 1;
+        for(int i = 0; i < teks.length(); i++){
+            if(Character.isUpperCase(teks.charAt(i)) || Character.isSpaceChar(teks.charAt(i)))
+                jumlahkata++;
+        }
+        return jumlahkata;
     }
 }
